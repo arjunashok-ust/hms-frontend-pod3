ec4f6f6263a76ddb0210a27cf0b83fc0d0fac302
Arjun <Arjun.Ashok@ust.com>
2026-05-16 01:06:24 +0530

    added login component

---
+++ src/app/login/login.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormGroup, Validators, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.htm',
+  styleUrl: './login.css',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+})
+
+export class LoginComponent {
+  loginForm: FormGroup;
+  constructor(readonly fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.minLength(8), Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+}
