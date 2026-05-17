70ed912599dfe0a8e6e6674d8d52439190f176be
Arjun <Arjun.Ashok@ust.com>
2026-05-18 00:21:37 +0530

    added splash screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { SignUpComponent } from './signup/signup';
+import { SplashComponent } from './splash/splash';
 
 export const routes: Routes = [
+  { path: '',component: SplashComponent},
   { path: 'login', component: LoginComponent },
   { path: 'signUp', component: SignUpComponent },
 ];
---
+++ src/app/splash/splash.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-splash',
+    templateUrl: './splash.htm',
+    styleUrl: './splash.css',
+    standalone: true,
+})
+
+export class SplashComponent{}
