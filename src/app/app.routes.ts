754bde15552050d1fc3af28d6b4c7ea8f27c6913
Arjun <Arjun.Ashok@ust.com>
2026-05-18 00:56:36 +0530

    added profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { SignUpComponent } from './signup/signup';
 import { SplashComponent } from './splash/splash';
+import { ProfileComponent } from './profile/profile';
 
 export const routes: Routes = [
-  { path: '',component: SplashComponent},
+  { path: '', component: SplashComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signUp', component: SignUpComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,2 +1,23 @@
-import { Component } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.htm',
+  styleUrl: './profile.css',
+  standalone: true,
+  imports: [CommonModule],
+})
+export class ProfileComponent {
+  user: UserData = new UserData();
+}
+
+class UserData {
+  employeeId: string = 'EMP-000001';
+  name: string = 'John Doe';
+  email: string = 'johndoe@hms.com';
+  roles: string[] = ['Owner', 'Doctor'];
+  department: string = 'OCD';
+  joiningDate: string = '12/03/2023';
+  medicalRegistrationNo: string = 'MED12345';
+}
